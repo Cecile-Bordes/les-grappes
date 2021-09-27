f1c5f6c11c6b46d7725df729cdf3f2c5044ba1a6
Cecile Bordes <bordes_cecile@hotmail.com>
2021-09-27 21:25:34 +0200

    Optimisations W3C et 3 forms validations javascript

--- main.js
+++ main.js
@@ -68,9 +68,14 @@ function btnOnclickvigneron() {
 }
 
 // FUNCTION : FORM VALIDATE
-let signin_form = document.querySelector('.form__login')
+//let signin_form = document.querySelector('.form__login')
+let signin_form = document.forms[0];
+let signin_form_pro = document.forms[1];
+let signin_form_vig = document.forms[2];
 
-let signin_btn = document.querySelector('.form--btn')
+let signin_btn = document.querySelector('#signin-btn')
+let signin_btn_pro = document.querySelector('#signin-btn-pro')
+let signin_btn_vig = document.querySelector('#signin-btn-vig')
 
 var validateEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -100,7 +105,7 @@ var checkSigninInput = (input) => {
             if (val.length === 0 || !validateEmail(val)) {
                 form_group.classList.add('err')
                 form_group.classList.remove('success')
-                err_span.innerHTML = 'Email non valide'
+                err_span.innerHTML = 'Saisissez un e-mail valide pour votre compte Particulier. '
             } else {
                 form_group.classList.add('success')
                 form_group.classList.remove('err')
@@ -109,22 +114,109 @@ var checkSigninInput = (input) => {
     }
 }
 
+var checkSigninInputPro = (input) => {
+  let err_span_pro = signin_form_pro.querySelector(`span[data-err-for="${input.id}"]`)
+  let val = input.value.trim()
+  let form_group = input.parentElement
+
+  switch(input.getAttribute('type')) {
+      case 'password':
+          if (val.length < 6) {
+              form_group.classList.add('err')
+              form_group.classList.remove('success')
+              err_span_pro.innerHTML = 'Le mot de passe doit contenir au moins de 6 caractères'
+          } else {
+              form_group.classList.add('success')
+              form_group.classList.remove('err')
+              err_span_pro.innerHTML = ''
+          }
+          break;
+      case 'email':
+          if (val.length === 0 || !validateEmail(val)) {
+              form_group.classList.add('err')
+              form_group.classList.remove('success')
+              err_span_pro.innerHTML = 'Email non valide pour vous connecter à votre compte Professionnel'
+          } else {
+              form_group.classList.add('success')
+              form_group.classList.remove('err')
+              err_span_pro.innerHTML = ''
+          }
+  }
+}
+
+var checkSigninInputVig = (input) => {
+  let err_span_vig = signin_form_vig.querySelector(`span[data-err-for="${input.id}"]`)
+  
+  let val = input.value.trim()
+  let form_group = input.parentElement
+
+  switch(input.getAttribute('type')) {
+      case 'password':
+          if (val.length < 6) {
+              form_group.classList.add('err')
+              form_group.classList.remove('success')
+              err_span_vig.innerHTML = 'Le mot de passe doit contenir au moins de 6 caractères'
+          } else {
+              form_group.classList.add('success')
+              form_group.classList.remove('err')
+              err_span_vig.innerHTML = ''
+          }
+          break;
+      case 'email':
+          if (val.length === 0 || !validateEmail(val)) {
+              form_group.classList.add('err')
+              form_group.classList.remove('success')
+              err_span_vig.innerHTML = 'Email non valide pour votre compte Vigneron'
+          } else {
+              form_group.classList.add('success')
+              form_group.classList.remove('err')
+              err_span_vig.innerHTML = ''
+          }
+  }
+}
 var checkSigninForm = () => {
     let inputs = signin_form.querySelectorAll('.form-input')
     inputs.forEach(input => checkSigninInput(input))
 }
 
+var checkSigninFormPro = () => {
+  let inputs_pro = signin_form_pro.querySelectorAll('.form-input')
+  inputs_pro.forEach(input => checkSigninInputPro(input))
+}
+
+var checkSigninFormVig = () => {
+  let inputs_vig = signin_form_vig.querySelectorAll('.form-input')
+  inputs_vig.forEach(input => checkSigninInputVig(input))
+}
+
 signin_btn.onclick = () => {
     checkSigninForm()
 }
+signin_btn_pro.onclick = () => {
+  checkSigninFormPro()
+}
+signin_btn_vig.onclick = () => {
+  checkSigninFormVig()
+}
 
 let inputs = signin_form.querySelectorAll('.form-input')
+let inputs_pro = signin_form_pro.querySelectorAll('.form-input')
+let inputs_vig = signin_form_vig.querySelectorAll('.form-input')
 inputs.forEach(input => {
     input.addEventListener('focusout', () => {
         checkSigninInput(input)
     })
 })
-
+inputs_pro.forEach(input => {
+  input.addEventListener('focusout', () => {
+      checkSigninInputPro(input)
+  })
+})
+inputs_vig.forEach(input => {
+  input.addEventListener('focusout', () => {
+      checkSigninInputVig(input)
+  })
+})
 
 // FUNCTION : INIT
 function init() {
